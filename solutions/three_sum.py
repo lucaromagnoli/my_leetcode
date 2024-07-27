470cf6ad1d4672e6ca15b28162b18fbbadf34327
lucaromagnoli <romagnoli.luca@gmail.com>
2024-07-27 15:32:46 +0100

    works but timesout

--- solutions/three_sum.py
+++ solutions/three_sum.py
@@ -32,32 +32,43 @@ Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
 """
+from collections import Counter
+from typing import Iterator
 
-from typing import List
+
+from collections import Counter
+from typing import Iterator
 
 
 class Solution:
-
-    def combinations(self, items, length):
-        if length == 0:
-            return [[]]
-        if len(items) == 0:
-            return []
-        results = []
-        for i in range(len(items)):
-            for combo in self.combinations(items[i + 1 :], length - 1):
-                results.append([items[i]] + combo)
-        return results
-
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        triplets = []
+    def iter_unique_pairs(self, nums_freq: Counter) -> Iterator[list[int]]:
         seen = set()
-        for combo in self.combinations(nums, 3):
-            key = "".join([str(c) for c in sorted(combo)])
-            print(key)
-            if key not in seen:
-                seen.add(key)
-                if sum(combo) == 0:
-                    triplets.append(combo)
+        for i_val, i_frq in nums_freq.items():
+            for j_val, j_frq in nums_freq.items():
+                if not (i_val == j_val and i_frq == j_frq == 1):
+                    pair = (i_val, j_val) if i_val <= j_val else (j_val, i_val)
+                    if pair not in seen:
+                        seen.add(pair)
+                        freq = i_frq - 1 if i_val == j_val else i_frq * j_frq
+                        yield pair, freq
 
-        return triplets
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums = tuple(nums)
+        sums = {}
+        results = []
+        nums_freq = Counter(nums)
+
+        for pair, freq in self.iter_unique_pairs(nums_freq):
+            num_sum = sum(pair)
+            sums.setdefault(num_sum, [])
+            sums[num_sum].extend(pair for _ in range(freq))
+
+        for num_val, num_freq in nums_freq.items():
+            diff = 0 - num_val
+            if diff in sums:
+                for pair in sums[diff]:
+                    trip = sorted(pair + (num_val,))
+                    trip_c = Counter(trip)
+                    if all(trip_c[n] <= nums_freq[n] for n in trip) and trip not in results:
+                        results.append(trip)
+        return results
